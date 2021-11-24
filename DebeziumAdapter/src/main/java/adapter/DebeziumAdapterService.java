7008e814280268362cfe988e21958796314a6633
yogesh-nextgen <birla.yogesh@tcs.com>
2021-11-24 19:18:49 +0530

    removed unnecessary configs

--- DebeziumAdapter/src/main/java/adapter/DebeziumAdapterService.java
+++ DebeziumAdapter/src/main/java/adapter/DebeziumAdapterService.java
@@ -2,9 +2,8 @@ package adapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
-@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
+@SpringBootApplication
 public class DebeziumAdapterService {
   public static void main(String[] args) {
     SpringApplication.run(DebeziumAdapterService.class, args);
