a05c3cf411b5efd28a3b5dd5e19f8638506bff85
Meric Feyzullahoglu <meric.feyzullahoglu@gmail.com>
2023-01-20 15:02:36 +0000

    Adapt Quarkus version of Scorpio AIO to Digital Twin
    Added a test profile which is enabled by default, which deploys
    All-in-one Runner of the Quarkus version of ScorpioBroker instead
    of the distributed version for development purposes.To disable,
    run the build script with TEST env set to FALSE, and run installation
    script after changing aaio.enabled variable in
    DigitalTwin/helm/charts/scorpio/values file to false.
    Deployment scripts, files and tests have been adapted accordingly.
    
    Closes: #177
    Closes: Open-IoT-Service-Platform/platform-launcher#653
    
    Signed-off-by: Meric Feyzullahoglu <meric.feyzullahoglu@gmail.com>

--- test/build-local-platform.sh
+++ test/build-local-platform.sh
@@ -13,14 +13,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+TEST="true"
 
 echo Build DT containers and push to local registry
 (cd .. && DOCKER_PREFIX=k3d-iff.localhost:12345 docker-compose build)
 (cd .. && DOCKER_PREFIX=k3d-iff.localhost:12345 docker-compose push)
 
 echo Build Scorpio containers
-( cd ../.. && git clone https://github.com/IndustryFusion/ScorpioBroker.git )
-( cd ../../ScorpioBroker && mvn clean package -DskipTests -Pdocker )
+( cd ../.. && rm -rf ScorpioBroker)
+
+if [[ $TEST -eq "true" ]]; then
+    ( cd ../.. && git clone https://github.com/IndustryFusion/ScorpioBroker.git)
+    ( cd ../../ScorpioBroker && git checkout ScorpioDigitalTwin )
+    ( cd ../../ScorpioBroker && source /etc/profile.d/maven.sh && mvn clean package -DskipTests -Ddocker -Din-memory -Pin-memory -Dquarkus.profile=in-memory -Dos=java)
+else
+    ( cd ../.. && git clone https://github.com/IndustryFusion/ScorpioBroker.git )
+    ( cd ../../ScorpioBroker && mvn clean package -DskipTests -Pdocker )
+fi
 docker images | tail -n +2 | awk '{print $1":"$2}'| grep ibn40 |
 {
     while read -r i; do
