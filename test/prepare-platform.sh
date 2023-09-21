12f0200152db768e83a5f77ffc3bbc2b86c57fa5
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2023-09-21 11:21:42 +0200

    Add device auth flow tests
    closes #424
    
    Signed-off-by: Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>

--- test/prepare-platform.sh
+++ test/prepare-platform.sh
@@ -22,7 +22,7 @@ SCRIPT_PATH=$(realpath $0)
 printf "\033[1mInstalling docker\n"
 printf -- "-----------------\033[0m\n"
 sudo apt -qq update
-sudo apt-get -qq install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get -qq install apt-transport-https ca-certificates curl gnupg-agent software-properties-common mosquitto-clients
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt -qq update
