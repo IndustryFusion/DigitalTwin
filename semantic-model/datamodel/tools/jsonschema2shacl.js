fcf297a5a1d71027da07a8a79abb64d94cd0c83f
marcel <wagmarcel@web.de>
2023-11-09 20:25:03 +0100

    cleaned up shacltools

--- semantic-model/datamodel/tools/jsonschema2shacl.js
+++ semantic-model/datamodel/tools/jsonschema2shacl.js
@@ -16,18 +16,10 @@
 'use strict'
 
 const $RefParser = require('json-schema-ref-parser')
-const $rdf = require('rdflib')
 const fs = require('fs')
 const yargs = require('yargs')
-const url = require('url')
 const ShaclUtils = require('./lib/shaclUtils')
-const ContextParser = require('jsonld-context-parser').ContextParser
-const ContextUtil = require('jsonld-context-parser').Util
-const myParser = new ContextParser()
 
-const RDF = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-const SHACL = $rdf.Namespace('http://www.w3.org/ns/shacl#')
-const IFFK = $rdf.Namespace('https://industry-fusion.org/knowledge/v0.1/')
 const argv = yargs
   .command('$0', 'Converting an IFF Schema file for NGSI-LD objects into a SHACL constraint.')
   .option('schema', {
@@ -54,94 +46,7 @@ const argv = yargs
 
 // Read in an array of JSON-Schemas
 const jsonSchemaText = fs.readFileSync(argv.s, 'utf8')
-const jsonSchema = JSON.parse(jsonSchemaText)
-let globalContext
-let globalPrefixHash
-
-function dumpShacl (nodeShape, store) {
-  dumpNodeShape(nodeShape, store)
-}
-
-function dumpNodeShape (nodeShape, store) {
-  const nodeName = decodeURIComponent(globalContext.expandTerm(nodeShape.targetClass))
-  const parsedUrl = new url.URL(nodeName)
-  const fragment = parsedUrl.hash.substring(1)
-  const shapeName = fragment + 'Shape'
-  store.add(IFFK(shapeName), RDF('type'), SHACL('NodeShape'))
-  store.add(IFFK(shapeName), SHACL('targetClass'), $rdf.sym(nodeName))
-  nodeShape.properties.forEach((property) => {
-    const propNode = $rdf.blankNode()
-    property.propertyNode = propNode
-    store.add(IFFK(shapeName), SHACL('property'), propNode)
-    dumpPropertyShape(property, store)
-  })
-}
-
-function dumpPropertyShape (propertyShape, store) {
-  const propNode = propertyShape.propertyNode
-  store.add(propNode, SHACL('minCount'), propertyShape.mincount)
-  store.add(propNode, SHACL('maxCount'), propertyShape.maxcount)
-  store.add(propNode, SHACL('nodeKind'), SHACL('BlankNode'))
-  store.add(propNode, SHACL('path'), propertyShape.path)
-  const attributeNode = $rdf.blankNode()
-  store.add(propNode, SHACL('property'), attributeNode)
-  const ngsildPrefix = globalPrefixHash['ngsi-ld']
-  const NGSILD = $rdf.Namespace(ngsildPrefix)
-  if (propertyShape.isProperty) {
-    store.add(attributeNode, SHACL('path'), NGSILD('hasValue'))
-  } else {
-    store.add(attributeNode, SHACL('path'), NGSILD('hasObject'))
-  }
-  store.add(attributeNode, SHACL('minCount'), 1)
-  store.add(attributeNode, SHACL('maxCount'), 1)
-  store.add(attributeNode, SHACL('nodeKind'), propertyShape.nodeKind)
-  const constraints = propertyShape.constraints
-  constraints.forEach((constraint) => {
-    store.add(attributeNode, constraint.type, constraint.params)
-  })
-}
-
-function encodeHash (id) {
-  const url = new URL(id)
-  const hash = encodeURIComponent(url.hash)
-  return `${url.protocol}//${url.hostname}${url.pathname}${hash}`
-}
-
-function shaclize (schemas, id) {
-  id = encodeHash(id)
-  const store = new $rdf.IndexedFormula()
-  const typeschema = schemas.find((schema) => schema.$id === id)
-  const nodeShape = ShaclUtils.scanNodeShape(typeschema, globalContext)
-  dumpShacl(nodeShape, store)
-  const serializer = new $rdf.Serializer(store)
-  serializer.setFlags('u')
-  serializer.setNamespaces(globalPrefixHash)
-  const turtle = serializer.statementsToN3(store.statementsMatching(undefined, undefined, undefined, undefined))
-  console.log(turtle)
-}
-
-async function loadContext (uriOrContext) {
-  const parseUrl = new url.URL(uriOrContext)
-  if (parseUrl.protocol === 'file:') {
-    uriOrContext = JSON.parse(fs.readFileSync(parseUrl.pathname, 'utf-8'))
-  }
-  const context = await myParser.parse(uriOrContext)
-  globalContext = context
-  const prefixHash = {}
-  Object.keys(context.getContextRaw()).filter((key) => key !== '@vocab').forEach((key) => {
-    const value = context.getContextRaw()[key]
-    if (typeof value === 'string') {
-      if (ContextUtil.isPrefixIriEndingWithGenDelim(value)) {
-        prefixHash[key] = value
-      }
-    } else if (typeof value === 'object') {
-      if (ContextUtil.isPrefixIriEndingWithGenDelim(value['@id'])) {
-        prefixHash[key] = value['@id']
-      }
-    }
-  })
-  globalPrefixHash = prefixHash
-}
+const jsonSchema = JSON.parse(jsonSchemaText);
 
 (async (jsconSchema) => {
   const myResolver = {
@@ -170,9 +75,9 @@ async function loadContext (uriOrContext) {
   }
 })(jsonSchema)
   .then(async (schema) => {
-    await loadContext(argv.c)
+    await ShaclUtils.loadContext(argv.c)
     return schema
   })
   .then(schema => {
-    shaclize(schema, argv.i)
+    ShaclUtils.shaclize(schema, argv.i)
   })
--- semantic-model/datamodel/tools/lib/shaclUtils.js
+++ semantic-model/datamodel/tools/lib/shaclUtils.js
@@ -15,12 +15,18 @@
 */
 'use strict'
 const $rdf = require('rdflib')
-const ContextParser = require('jsonld-context-parser').ContextParser
 const ContextUtil = require('jsonld-context-parser').Util
+const url = require('url')
+const fs = require('fs')
+const ContextParser = require('jsonld-context-parser').ContextParser
+const myParser = new ContextParser()
 
 const RDF = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
 const SHACL = $rdf.Namespace('http://www.w3.org/ns/shacl#')
 const IFFK = $rdf.Namespace('https://industry-fusion.org/knowledge/v0.1/')
+
+let globalContext
+let globalPrefixHash
 
 class NodeShape {
   constructor (targetClass) {
@@ -71,15 +77,54 @@ class Constraint {
   }
 }
 
-function scanNodeShape (typeschema, globalContext) {
+function dumpPropertyShape (propertyShape, store) {
+  const propNode = propertyShape.propertyNode
+  store.add(propNode, SHACL('minCount'), propertyShape.mincount)
+  store.add(propNode, SHACL('maxCount'), propertyShape.maxcount)
+  store.add(propNode, SHACL('nodeKind'), SHACL('BlankNode'))
+  store.add(propNode, SHACL('path'), propertyShape.path)
+  const attributeNode = $rdf.blankNode()
+  store.add(propNode, SHACL('property'), attributeNode)
+  const ngsildPrefix = globalPrefixHash['ngsi-ld']
+  const NGSILD = $rdf.Namespace(ngsildPrefix)
+  if (propertyShape.isProperty) {
+    store.add(attributeNode, SHACL('path'), NGSILD('hasValue'))
+  } else {
+    store.add(attributeNode, SHACL('path'), NGSILD('hasObject'))
+  }
+  store.add(attributeNode, SHACL('minCount'), 1)
+  store.add(attributeNode, SHACL('maxCount'), 1)
+  store.add(attributeNode, SHACL('nodeKind'), propertyShape.nodeKind)
+  const constraints = propertyShape.constraints
+  constraints.forEach((constraint) => {
+    store.add(attributeNode, constraint.type, constraint.params)
+  })
+}
+
+function dumpNodeShape (nodeShape, store) {
+  const nodeName = decodeURIComponent(globalContext.expandTerm(nodeShape.targetClass))
+  const parsedUrl = new url.URL(nodeName)
+  const fragment = parsedUrl.hash.substring(1)
+  const shapeName = fragment + 'Shape'
+  store.add(IFFK(shapeName), RDF('type'), SHACL('NodeShape'))
+  store.add(IFFK(shapeName), SHACL('targetClass'), $rdf.sym(nodeName))
+  nodeShape.properties.forEach((property) => {
+    const propNode = $rdf.blankNode()
+    property.propertyNode = propNode
+    store.add(IFFK(shapeName), SHACL('property'), propNode)
+    dumpPropertyShape(property, store)
+  })
+}
+
+function scanNodeShape (typeschema) {
   const id = typeschema.$id
 
   const nodeShape = new NodeShape(id)
-  scanProperties(nodeShape, typeschema, globalContext)
+  scanProperties(nodeShape, typeschema)
   return nodeShape
 }
 
-function scanProperties (nodeShape, typeschema, globalContext) {
+function scanProperties (nodeShape, typeschema) {
   let required = []
   if ('required' in typeschema) {
     required = typeschema.required
@@ -118,7 +163,7 @@ function scanProperties (nodeShape, typeschema, globalContext) {
   }
   if ('allOf' in typeschema) {
     typeschema.allOf.forEach((elem) => {
-      scanProperties(nodeShape, elem, globalContext)
+      scanProperties(nodeShape, elem)
     })
   }
 }
@@ -152,9 +197,58 @@ function scanConstraints (propertyShape, typeschema) {
   }
 }
 
+function shaclize (schemas, id) {
+  id = encodeHash(id)
+  const store = new $rdf.IndexedFormula()
+  const typeschema = schemas.find((schema) => schema.$id === id)
+  const nodeShape = scanNodeShape(typeschema)
+  dumpShacl(nodeShape, store)
+  const serializer = new $rdf.Serializer(store)
+  serializer.setFlags('u')
+  serializer.setNamespaces(globalPrefixHash)
+  const turtle = serializer.statementsToN3(store.statementsMatching(undefined, undefined, undefined, undefined))
+  console.log(turtle)
+}
+
+function encodeHash (id) {
+  const url = new URL(id)
+  const hash = encodeURIComponent(url.hash)
+  return `${url.protocol}//${url.hostname}${url.pathname}${hash}`
+}
+
+function dumpShacl (nodeShape, store) {
+  dumpNodeShape(nodeShape, store)
+}
+
+async function loadContext (uriOrContext) {
+  const parseUrl = new url.URL(uriOrContext)
+  if (parseUrl.protocol === 'file:') {
+    uriOrContext = JSON.parse(fs.readFileSync(parseUrl.pathname, 'utf-8'))
+  }
+  const context = await myParser.parse(uriOrContext)
+  globalContext = context
+  const prefixHash = {}
+  Object.keys(context.getContextRaw()).filter((key) => key !== '@vocab').forEach((key) => {
+    const value = context.getContextRaw()[key]
+    if (typeof value === 'string') {
+      if (ContextUtil.isPrefixIriEndingWithGenDelim(value)) {
+        prefixHash[key] = value
+      }
+    } else if (typeof value === 'object') {
+      if (ContextUtil.isPrefixIriEndingWithGenDelim(value['@id'])) {
+        prefixHash[key] = value['@id']
+      }
+    }
+  })
+  globalPrefixHash = prefixHash
+}
+
 module.exports = {
   NodeShape,
   PropertyShape,
   Constraint,
-  scanNodeShape
+  scanNodeShape,
+  dumpNodeShape,
+  shaclize,
+  loadContext
 }
