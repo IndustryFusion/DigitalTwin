9de755e4d300f86f35e961e146880f3f88e6bcd7
marcel <wagmarcel@web.de>
2023-12-09 14:16:21 +0100

    updated get-device-token

--- NgsildTestAgent/util/get-device-token.sh
+++ NgsildTestAgent/util/get-device-token.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-# Author: Yatindra shashi
-# Brief: Script to get device token to connect device to mqtt server
-# Description: Dependend on Environment variables get device onboarding token
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+set -e
+
+
 
 # Define the JSON file path
 onboard_token_json_file="../data/onboard-token.json"
@@ -18,7 +32,6 @@ if [ ! -f "$onboard_token_json_file" ]; then
     exit 1
 fi
 
-echo "If you are running on local host, please check if you already run script ../test/setup-local-ingress.sh"
 access_token=$(jq -r '.access_token' "$onboard_token_json_file")
 if [ -z "$access_token" ]; then
     echo "access_token not found, please check again"
@@ -43,6 +56,11 @@ response_token=$(curl -X POST "$ONBOARDING_TOKEN_ENDPOINT" -d "client_id=device-
 -d "grant_type=urn:ietf:params:oauth:grant-type:token-exchange" -d "requested_token_type=urn:ietf:params:oauth:token-type:refresh_token" \
 -d "audience=device" -H "X-GatewayID: $gatewayid" -H "X-DeviceID: $deviceid" -H "X-Access-Type: device" -H "X-DeviceUID: uid" | jq '.')
 echo "response token" $response_token
+if [ -n "$(echo $response_token | jq '.error') |  tr -d '[:space:]'" ]; then
+    echo "Error: Invalid onbarding token found."
+    echo $response_token
+    exit 1
+fi
 
 updated_json_data=$(jq --argjson response "$response_token" '. += $response' "$dev_json_file")
 echo "$updated_json_data" > "$dev_json_file"
