81a2ef861a9230c69d7524760ac6457e8dc14fbf
yshashix <yatindra.shashi@intel.com>
2024-09-19 17:16:01 +0200

    added support for ubuntu 24.04 and 22.04 with updated docker v27.2. Updated github workflows buld with 24.04 as well

--- test/prepare-platform.sh
+++ test/prepare-platform.sh
@@ -23,10 +23,15 @@ printf "\033[1mInstalling docker\n"
 printf -- "-----------------\033[0m\n"
 sudo apt -qq update
 sudo apt-get -qq install apt-transport-https ca-certificates curl gnupg-agent software-properties-common mosquitto-clients postgresql-client-common postgresql-client
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the docker repository to Apt sources:
+echo \
+  "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt -qq update
-sudo apt-get install -y docker-ce=5:23.0.1-1~ubuntu.$(lsb_release -sr)~$(lsb_release -cs) docker-ce-cli=5:23.0.1-1~ubuntu.$(lsb_release -sr)~$(lsb_release -cs) containerd.io
+sudo apt-get install -y docker-ce=5:27.2.1-1~ubuntu.$(lsb_release -sr)~$(lsb_release -cs) docker-ce-cli=5:27.2.1-1~ubuntu.$(lsb_release -sr)~$(lsb_release -cs) containerd.io
 printf "\033[1mSuccessfully installed %s\033[0m\n" "$(docker --version)"
 printf "\n"
 
